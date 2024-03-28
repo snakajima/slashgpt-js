54f4216db4c5eb0040d92bce4238c389ce81fe43
isamu <isamu@to-kyo.to>
2024-03-28 20:58:48 +0900

    add test common file

---
+++ tests/common.ts
@@ -0,0 +1,42 @@
+import fs from "fs";
+import YAML from "yaml";
+import path from "path";
+
+import { print_bot } from "../src/chat_utils";
+
+export const readJsonManifest = (fileName: string) => {
+  const manifest_file = fs.readFileSync(fileName, "utf8");
+  const manifest = JSON.parse(manifest_file);
+  return manifest;
+};
+
+export const readYamlManifest = (fileName: string) => {
+  const manifest_file = fs.readFileSync(fileName, "utf8");
+  const manifest = YAML.parse(manifest_file);
+  return manifest;
+};
+
+export const readManifestData = (fileName: string) => {
+  const file = getFilePath(fileName);
+  if (file.endsWith(".yaml") || file.endsWith(".yml")) {
+    return readYamlManifest(file);
+  }
+  if (file.endsWith(".json")) {
+    return readJsonManifest(file);
+  }
+  throw new Error("No file exists " + fileName);
+};
+
+export const callback = (callback_type: string, data: string) => {
+  if (callback_type === "bot") {
+    print_bot("bot", data);
+  }
+};
+
+export const getBasePath = () => path.resolve(__dirname + "/../");
+
+const getFilePath = (fileName: string) => {
+  const base_path = getBasePath();
+  const file = base_path + "/manifests/main/" + fileName;
+  return file;
+};
