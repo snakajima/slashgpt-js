1fe0953f85c08075f751e085f449c05bf18fff0c
isamu <isamu@to-kyo.to>
2024-04-01 21:12:26 +0900

    update

--- lib/simple_client.d.ts
+++ lib/simple_client.d.ts
@@ -1,4 +1,3 @@
 import { ChatSession } from "./";
-export declare const getCurrentFilePath: () => string;
 export declare const callback: (callback_type: string, data: unknown) => void;
-export declare const justRun: (fileName: string) => Promise<ChatSession>;
+export declare const justRun: (fileFullName: string) => Promise<ChatSession>;
