5a1aae2e6bbf6f0e3eaf0da63957ee39a9498e23
snakajima <satoshi.nakajima@gmail.com>
2024-04-03 04:46:35 -1000

    FlowData

--- src/flow.ts
+++ src/flow.ts
@@ -1,11 +1,20 @@
+type Node = {
+  title: string;
+}
+
 type FlowData = {
   title: string;
+  nodes: Record<string, Node>
 };
+
 
 class Flow {
   public data: FlowData;
+  public nodes: Record<string, Node>
+
   constructor(data: FlowData) {
     this.data = data;
+    this.nodes = data.nodes;
   }
 }
 
--- tests/sample_flow.ts
+++ tests/sample_flow.ts
@@ -7,6 +7,6 @@ const main = async () => {
   const file_path = path.resolve(__dirname) + "/flow.yml";
   const flow_data = readManifestData(file_path);
   const flow = new Flow(flow_data)
-  console.log(flow.data);
+  console.log(flow.nodes);
 };
 main();
