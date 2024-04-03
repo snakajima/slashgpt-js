2768d2211ccba1833d5f8bc7a2f09d3d6e3e48c0
snakajima <satoshi.nakajima@gmail.com>
2024-04-03 05:30:49 -1000

    setTimeout

--- tests/sample_flow.ts
+++ tests/sample_flow.ts
@@ -7,11 +7,12 @@ const main = async () => {
   const file_path = path.resolve(__dirname) + "/flow.yml";
   const flow_data = readManifestData(file_path);
   const flow = new Flow(flow_data)
-  console.log(flow.title);
-  console.log(flow.nodes);
   flow.run(async (result) => {
     if (result.cmd == FlowCommand.Execute) {
-        console.log(result)
+        console.log("executing", result.node)
+        setTimeout(() => {
+          console.log("done")
+        }, 500);
     }
   });
 };
