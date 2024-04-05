91e49901096bf2fcced8951aa07b5cdf4ee4318e
snakajima <satoshi.nakajima@gmail.com>
2024-04-04 17:15:30 -1000

    graphs

--- tests/sample_flow.ts
+++ tests/sample_flow.ts
@@ -4,7 +4,7 @@ import { Graph, FlowCommand } from "../src/flow";
 import { readManifestData } from "../src/file_utils";
 
 const main = async () => {
-  const file_path = path.resolve(__dirname) + "/flow.yml";
+  const file_path = path.resolve(__dirname) + "/graphs/sample1.yml";
   const graph_data = readManifestData(file_path);
   const graph = new Graph(graph_data)
   graph.run(async (result) => {
