3fc8f4ec0d4e40d51903ac766fd265821a36f7f3
snakajima <satoshi.nakajima@gmail.com>
2024-04-05 08:01:30 -0700

    use Promise

--- tests/sample_flow.ts
+++ tests/sample_flow.ts
@@ -3,36 +3,35 @@ import { Graph, FlowCommand } from "../src/flow";
 
 import { readManifestData } from "../src/file_utils";
 
-type OnComplete = (graph: Graph) => void;
-
-const test = async (graph_data: any, callback: OnComplete) => {
-  const graph = new Graph(graph_data, async (params) => {
-    if (params.cmd == FlowCommand.Execute) {
-        const node = params.node;
-        console.log("executing", node, params.params)
-        setTimeout(() => {
-          if (params.params.fail) {
-            const result = { [node]:"failed" };
-            console.log("failed", node, result)
-            graph.reportError(node, result);
-          } else {
-            const result = { [node]:"success" };
-            console.log("completing", node, result)
-            graph.feed(node, result)
-          }
-        }, params.params.delay);
-    } else if (params.cmd == FlowCommand.OnComplete) {
-      callback(graph);
-    }
+const test = async (graph_data: any) => {
+  return new Promise((resolve, reject) => {
+    const graph = new Graph(graph_data, async (params) => {
+      if (params.cmd == FlowCommand.Execute) {
+          const node = params.node;
+          console.log("executing", node, params.params)
+          setTimeout(() => {
+            if (params.params.fail) {
+              const result = { [node]:"failed" };
+              console.log("failed", node, result)
+              graph.reportError(node, result);
+            } else {
+              const result = { [node]:"success" };
+              console.log("completing", node, result)
+              graph.feed(node, result)
+            }
+          }, params.params.delay);
+      } else if (params.cmd == FlowCommand.OnComplete) {
+        resolve(graph);
+      }
+    });
+    graph.run();
   });
-  graph.run();
 }
 
 const main = async () => {
   const file_path = path.resolve(__dirname) + "/graphs/sample1.yml";
   const graph_data = readManifestData(file_path);
-  test(graph_data, (graph) => {
-    console.log("=== ON COMPLETE")
-  });
+  await test(graph_data);
+  console.log("COMPLETE");
 };
 main();
