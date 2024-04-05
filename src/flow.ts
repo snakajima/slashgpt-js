b23b1058649ecec515ff997eb8c54d6892fe715f
snakajima <satoshi.nakajima@gmail.com>
2024-04-04 18:34:54 -1000

    no title

--- src/flow.ts
+++ src/flow.ts
@@ -6,13 +6,11 @@ export enum NodeState {
 }
 
 type NodeData = {
-  title: string;
   inputs: undefined | Record<string, any>;
   params: any;
 }
 
 type FlowData = {
-  title: string;
   nodes: Record<string, NodeData>
 };
 
@@ -25,7 +23,6 @@ export enum FlowCommand {
 
 class Node {
   public key: string;
-  public title: string;
   public inputs: Record<string, any>
   public pendings: Set<string>;
   public params: any;
@@ -34,7 +31,6 @@ class Node {
   public result: Record<string, any>;
   constructor(key: string, data: NodeData) {
     this.key = key;
-    this.title = data.title;
     this.inputs = data.inputs ?? {};
     this.pendings = new Set(Object.keys(this.inputs));
     this.params = data.params;
@@ -49,12 +45,10 @@ class Node {
 }
 
 export class Graph {
-  public title: string;
   public nodes: Record<string, Node>
   public callback: FlowCallback;
 
   constructor(data: FlowData, callback: FlowCallback) {
-    this.title = data.title;
     this.callback = callback;
     const foo: Record<string, Node> = {}; // HACK: Work around
     this.nodes = Object.keys(data.nodes).reduce((nodes, key) => {
